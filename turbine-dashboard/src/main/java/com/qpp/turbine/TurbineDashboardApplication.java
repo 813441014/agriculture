94a31427c5f28b5100e4810ad486030efd1195a8
qpp <1074723451@qq.com>
2019-01-04 22:22:51 +0800

    turbine-dashbord 断路器监控器

---
+++ turbine-dashboard/src/main/java/com/qpp/turbine/TurbineDashboardApplication.java
@@ -0,0 +1,20 @@
+package com.qpp.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableTurbine
+@EnableHystrixDashboard
+public class TurbineDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineDashboardApplication.class, args);
+    }
+
+}
+
